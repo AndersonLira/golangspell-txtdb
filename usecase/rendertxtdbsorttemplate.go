3c2a5c1847ad4b08884bd60218764a76b80f2f42
Anderson Lira <anderson.lira@celfocus.com>
2020-09-14 15:13:51 +0100

    operator login added. txtdbsort command

--- usecase/rendertxtdbsorttemplate.go
+++ usecase/rendertxtdbsorttemplate.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	
 	"github.com/andersonlira/goutils/io"
@@ -14,6 +15,14 @@ func RendertxtdbsortTemplate(args []string) error {
 	domainEntity := args[0]
 	field := strcase.ToCamel(args[1])
 	domainSnake := strcase.ToSnake(domainEntity)
+
+	operator := "<"
+
+	if len(args) > 2 {
+		if b, _ := strconv.ParseBool(args[2]); b {
+			operator = ">"
+		}
+	}
 	
 	coder, err := recode.MakeCoder(fmt.Sprintf("./gateway/txtdb/%s_repository.go", domainSnake))
 	if err != nil {
@@ -22,7 +31,7 @@ func RendertxtdbsortTemplate(args []string) error {
 	
 
 	line1 := "\tsort.Slice(list, func(i, j int) bool {"
-	line2 := fmt.Sprintf("\t\treturn list[i].%s < list[j].%s",field,field)
+	line2 := fmt.Sprintf("\t\treturn list[i].%s %s list[j].%s",field,operator,field)
 	line3 := "\t})"
 	coder.AddAfterLine(fmt.Sprintf("func Get%sList() []domain.%s",domainEntity,domainEntity),"return list",line1,line2,line3)
 
